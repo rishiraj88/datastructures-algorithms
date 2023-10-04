b6395f21d53207235f031fa2d635dff3f9815c60
Rishi Raj <rishiraj88+github@gmail.com>
2023-10-04 14:27:48 +0200

    Updated the selection sort tester.

--- SelectionSort.java
+++ SelectionSort.java
@@ -1,4 +1,6 @@
+import java.sql.SQLOutput;
 import java.util.Arrays;
+import java.util.Random;
 
 public class SelectionSort {
 
@@ -21,7 +23,24 @@ public class SelectionSort {
     public static void main(String[] args) {
         int[] a1 = {1,2,4,6,23,1,3,5};
         System.out.println("Input array: "+ Arrays.toString(a1));
+        long start = System.currentTimeMillis();
         System.out.println("Sorted array: "+Arrays.toString(sort(a1)) );
+        long end = System.currentTimeMillis();
+        System.out.println("Time taken to sort: "+(end-start) +"ms.");
+
+        Random random = new Random();
+        a1 = new int[300000];
+        for(int i=0;i<a1.length;i++) {
+            a1[i] = random.nextInt(100000);
+            System.out.println(a1[i]);
+        }
+        System.out.println("Input array: "+ Arrays.toString(a1));
+        start = System.currentTimeMillis();
+        System.out.println("Sorted array: "+Arrays.toString(sort(a1)) );
+        end = System.currentTimeMillis();
+        System.out.println("Time taken to sort: "+(end-start) +"ms.");
+
+
 
     }
 
